a838bc9e4e5bc2c9570cf3fbdbc28c39038d11f6
paritytech-ci <devops-team@parity.io>
2022-10-03 16:50:14 +0000

    Updated docs for master

---
+++ doc/implementors/pallet_referenda/types/trait.TracksInfo.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["kusama_runtime"] = [{"text":"impl TracksInfo&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u128.html\">u128</a>, <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"kusama_runtime/governance/struct.TracksInfo.html\" title=\"struct kusama_runtime::governance::TracksInfo\">TracksInfo</a>","synthetic":false,"types":["kusama_runtime::governance::tracks::TracksInfo"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/kusama_runtime_constants/currency/sidebar-items.js
+++ doc/kusama_runtime_constants/currency/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["CENTS",""],["EXISTENTIAL_DEPOSIT","The existential deposit."],["GRAND",""],["MILLICENTS",""],["UNITS",""]],"fn":[["deposit",""]]};
+window.SIDEBAR_ITEMS = {"constant":[["CENTS",""],["EXISTENTIAL_DEPOSIT","The existential deposit."],["GRAND",""],["MILLICENTS",""],["QUID",""],["UNITS",""]],"fn":[["deposit",""]]};
