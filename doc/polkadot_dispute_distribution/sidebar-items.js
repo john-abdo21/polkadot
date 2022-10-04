f81450f6b88653e50fc5ea61df2c413da2845b04
paritytech-ci <devops-team@parity.io>
2022-10-04 17:02:23 +0000

    Updated docs for master

--- doc/polkadot_dispute_distribution/sidebar-items.js
+++ doc/polkadot_dispute_distribution/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["DisputeDistributionSubsystem","The dispute distribution subsystem."],["Metrics","Dispute Distribution metrics."]]};
+window.SIDEBAR_ITEMS = {"constant":[["RECEIVE_RATE_LIMIT","Rate limit on the `receiver` side."],["SEND_RATE_LIMIT","Rate limit on the `sender` side."]],"struct":[["DisputeDistributionSubsystem","The dispute distribution subsystem."],["Metrics","Dispute Distribution metrics."]]};
