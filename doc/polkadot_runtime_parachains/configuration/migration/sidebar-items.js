2576920bd63b7d592600b082f34ec3ceee9cdd2b
paritytech-ci <devops-team@parity.io>
2022-10-05 10:03:40 +0000

    Updated docs for master

--- doc/polkadot_runtime_parachains/configuration/migration/sidebar-items.js
+++ doc/polkadot_runtime_parachains/configuration/migration/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["STORAGE_VERSION","The current storage version."]]};
+window.SIDEBAR_ITEMS = {"constant":[["STORAGE_VERSION","The current storage version."]],"mod":[["v3",""]]};
