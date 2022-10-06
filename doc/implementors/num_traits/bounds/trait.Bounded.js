1b7f512078b83d6326bb9c518f1d9b717ef246cb
paritytech-ci <devops-team@parity.io>
2022-10-06 17:37:59 +0000

    Updated docs for master

--- doc/implementors/num_traits/bounds/trait.Bounded.js
+++ doc/implementors/num_traits/bounds/trait.Bounded.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["polkadot_runtime_common"] = [];
 implementors["polkadot_service"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
