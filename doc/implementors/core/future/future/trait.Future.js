0af5052a171655fef1f4b253269a8a441049013f
paritytech-ci <devops-team@parity.io>
2022-10-10 06:21:38 +0000

    Updated docs for master

--- doc/implementors/core/future/future/trait.Future.js
+++ doc/implementors/core/future/future/trait.Future.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["polkadot_node_subsystem"] = [];
+implementors["polkadot_node_subsystem_test_helpers"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"polkadot_node_subsystem_test_helpers/struct.Yield.html\" title=\"struct polkadot_node_subsystem_test_helpers::Yield\">Yield</a>","synthetic":false,"types":["polkadot_node_subsystem_test_helpers::Yield"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
