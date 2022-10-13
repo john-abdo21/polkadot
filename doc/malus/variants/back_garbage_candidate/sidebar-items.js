ddee6266b69317a94e8797eadd581dd81dcabbcc
paritytech-ci <devops-team@parity.io>
2022-10-13 10:07:36 +0000

    Updated docs for master

--- doc/malus/variants/back_garbage_candidate/sidebar-items.js
+++ doc/malus/variants/back_garbage_candidate/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["BackGarbageCandidate","Generates an overseer that replaces the candidate validation subsystem with our malicious variant."]]};
+window.SIDEBAR_ITEMS = {"struct":[["BackGarbageCandidateOptions",""],["BackGarbageCandidates","Generates an overseer that replaces the candidate validation subsystem with our malicious variant."]]};
--- doc/malus/variants/suggest_garbage_candidate/sidebar-items.js
+++ doc/malus/variants/suggest_garbage_candidate/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["BackGarbageCandidateWrapper","Garbage candidate implementation wrapper which implements `OverseerGen` glue."],["Inner",""],["NoteCandidate","Replace outgoing approval messages with disputes."]]};
+window.SIDEBAR_ITEMS = {"struct":[["NoteCandidate","Replace outgoing approval messages with disputes."],["SuggestGarbageCandidateOptions",""],["SuggestGarbageCandidates","Garbage candidate implementation wrapper which implements `OverseerGen` glue."]]};
