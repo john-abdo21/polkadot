539430d556790039804eca346a558f079a7d763b
paritytech-ci <devops-team@parity.io>
2022-12-08 13:43:19 +0000

    Updated docs for master

--- doc/rococo_runtime_constants/weights/block_weights/sidebar-items.js
+++ doc/rococo_runtime_constants/weights/block_weights/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["BlockExecutionWeight","Time to execute an empty block. Calculated by multiplying the Average with `1` and adding `0`."]]};
+window.SIDEBAR_ITEMS = {"struct":[["BlockExecutionWeight","Time to execute an empty block. Calculated by multiplying the Average with `1.0` and adding `0`."]]};
