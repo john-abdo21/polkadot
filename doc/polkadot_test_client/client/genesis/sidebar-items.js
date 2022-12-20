1ffce6e884c5d844e514caf8e180632cc13b929d
paritytech-ci <devops-team@parity.io>
2022-12-20 08:35:36 +0000

    Updated docs for master

--- doc/polkadot_test_client/client/genesis/sidebar-items.js
+++ doc/polkadot_test_client/client/genesis/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["construct_genesis_block","Create a genesis block, given the initial storage."]]};
+window.SIDEBAR_ITEMS = {"fn":[["construct_genesis_block","Create a genesis block, given the initial storage."]],"struct":[["GenesisBlockBuilder","Default genesis block builder in Substrate."]],"trait":[["BuildGenesisBlock","Trait for building the genesis block."]]};
