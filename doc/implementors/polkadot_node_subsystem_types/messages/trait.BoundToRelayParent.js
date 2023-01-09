51b00c522617beedd2fad4465107443297cc2678
paritytech-ci <devops-team@parity.io>
2023-01-09 12:29:05 +0000

    Updated docs for master

--- doc/implementors/polkadot_node_subsystem_types/messages/trait.BoundToRelayParent.js
+++
@@ -1,5 +0,0 @@
-(function() {var implementors = {
-"malus":[],
-"polkadot_node_subsystem":[],
-"polkadot_node_subsystem_types":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
