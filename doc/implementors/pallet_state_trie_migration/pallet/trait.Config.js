695a81d5c9edfefb2abdbbb5acaa109d76919862
paritytech-ci <devops-team@parity.io>
2023-02-22 14:49:07 +0000

    Updated docs for master

--- doc/implementors/pallet_state_trie_migration/pallet/trait.Config.js
+++ doc/implementors/pallet_state_trie_migration/pallet/trait.Config.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"rococo_runtime":[["impl Config for <a class=\"struct\" href=\"rococo_runtime/struct.Runtime.html\" title=\"struct rococo_runtime::Runtime\">Runtime</a>"]],
-"westend_runtime":[["impl Config for <a class=\"struct\" href=\"westend_runtime/struct.Runtime.html\" title=\"struct westend_runtime::Runtime\">Runtime</a>"]]
+"rococo_runtime":[["impl Config for <a class=\"struct\" href=\"rococo_runtime/struct.Runtime.html\" title=\"struct rococo_runtime::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
