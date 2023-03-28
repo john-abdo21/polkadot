e45057aa6c3c6c6c44ef22b17f88bf0138607ae5
paritytech-ci <devops-team@parity.io>
2023-03-28 14:04:48 +0000

    Updated docs for master

--- doc/polkadot_runtime_metrics/sidebar-items.js
+++ doc/polkadot_runtime_metrics/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["Counter","A dummy `Counter`."],["CounterVec","A dummy `CounterVec`."]]};
+window.SIDEBAR_ITEMS = {"fn":[["get_current_time","Dummy implementation - always 0"]],"struct":[["Counter","A dummy `Counter`."],["CounterVec","A dummy `CounterVec`."],["Histogram","A dummy `Histogram`"]]};
