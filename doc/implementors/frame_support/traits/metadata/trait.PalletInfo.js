6567231e95bd1d79bf63bf06039c8791dd2a8cc2
paritytech-ci <devops-team@parity.io>
2023-05-11 18:15:09 +0000

    Updated docs for master

--- doc/implementors/frame_support/traits/metadata/trait.PalletInfo.js
+++ doc/implementors/frame_support/traits/metadata/trait.PalletInfo.js
@@ -4,5 +4,5 @@
 "polkadot_test_runtime":[["impl PalletInfo for <a class=\"struct\" href=\"polkadot_test_runtime/struct.PalletInfo.html\" title=\"struct polkadot_test_runtime::PalletInfo\">PalletInfo</a>"]],
 "rococo_runtime":[["impl PalletInfo for <a class=\"struct\" href=\"rococo_runtime/struct.PalletInfo.html\" title=\"struct rococo_runtime::PalletInfo\">PalletInfo</a>"]],
 "westend_runtime":[["impl PalletInfo for <a class=\"struct\" href=\"westend_runtime/struct.PalletInfo.html\" title=\"struct westend_runtime::PalletInfo\">PalletInfo</a>"]],
-"xcm_fuzzer":[["impl PalletInfo for <a class=\"struct\" href=\"xcm_fuzzer/parachain/struct.PalletInfo.html\" title=\"struct xcm_fuzzer::parachain::PalletInfo\">PalletInfo</a>"],["impl PalletInfo for <a class=\"struct\" href=\"xcm_fuzzer/relay_chain/struct.PalletInfo.html\" title=\"struct xcm_fuzzer::relay_chain::PalletInfo\">PalletInfo</a>"]]
+"xcm_fuzzer":[["impl PalletInfo for <a class=\"struct\" href=\"xcm_fuzzer/relay_chain/struct.PalletInfo.html\" title=\"struct xcm_fuzzer::relay_chain::PalletInfo\">PalletInfo</a>"],["impl PalletInfo for <a class=\"struct\" href=\"xcm_fuzzer/parachain/struct.PalletInfo.html\" title=\"struct xcm_fuzzer::parachain::PalletInfo\">PalletInfo</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
