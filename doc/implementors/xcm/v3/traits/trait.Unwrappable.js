a3421e849d9049118f9f000c690ce9a6c551c8d9
paritytech-ci <devops-team@parity.io>
2023-06-25 12:10:12 +0000

    Updated docs for master

--- doc/implementors/xcm/v3/traits/trait.Unwrappable.js
+++
@@ -1,5 +0,0 @@
-(function() {var implementors = {
-"xcm":[],
-"xcm_builder":[],
-"xcm_simulator":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
