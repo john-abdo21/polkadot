c83c4223095606462d523842be8829dfb4a7bb55
paritytech-ci <devops-team@parity.io>
2023-06-28 18:05:34 +0000

    Updated docs for master

--- doc/implementors/sp_runtime/traits/trait.ValidateUnsigned.js
+++ doc/implementors/sp_runtime/traits/trait.ValidateUnsigned.js
@@ -7,5 +7,5 @@
 "polkadot_test_runtime":[["impl ValidateUnsigned for <a class=\"struct\" href=\"polkadot_test_runtime/struct.Runtime.html\" title=\"struct polkadot_test_runtime::Runtime\">Runtime</a>"]],
 "rococo_runtime":[["impl ValidateUnsigned for <a class=\"struct\" href=\"rococo_runtime/struct.Runtime.html\" title=\"struct rococo_runtime::Runtime\">Runtime</a>"]],
 "westend_runtime":[["impl ValidateUnsigned for <a class=\"struct\" href=\"westend_runtime/struct.Runtime.html\" title=\"struct westend_runtime::Runtime\">Runtime</a>"]],
-"xcm_fuzzer":[["impl ValidateUnsigned for <a class=\"struct\" href=\"xcm_fuzzer/parachain/struct.Runtime.html\" title=\"struct xcm_fuzzer::parachain::Runtime\">Runtime</a>"],["impl ValidateUnsigned for <a class=\"struct\" href=\"xcm_fuzzer/relay_chain/struct.Runtime.html\" title=\"struct xcm_fuzzer::relay_chain::Runtime\">Runtime</a>"]]
+"xcm_fuzzer":[["impl ValidateUnsigned for <a class=\"struct\" href=\"xcm_fuzzer/relay_chain/struct.Runtime.html\" title=\"struct xcm_fuzzer::relay_chain::Runtime\">Runtime</a>"],["impl ValidateUnsigned for <a class=\"struct\" href=\"xcm_fuzzer/parachain/struct.Runtime.html\" title=\"struct xcm_fuzzer::parachain::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
