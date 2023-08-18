74a46f546d80d0f83c3e38badb1e9d2510ef5d14
paritytech-ci <devops-team@parity.io>
2023-08-18 09:37:19 +0000

    Updated docs for master

--- doc/implementors/pallet_elections_phragmen/pallet/trait.Config.js
+++ doc/implementors/pallet_elections_phragmen/pallet/trait.Config.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"polkadot_runtime":[["impl Config for <a class=\"struct\" href=\"polkadot_runtime/struct.Runtime.html\" title=\"struct polkadot_runtime::Runtime\">Runtime</a>"]],
 "rococo_runtime":[["impl Config for <a class=\"struct\" href=\"rococo_runtime/struct.Runtime.html\" title=\"struct rococo_runtime::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
